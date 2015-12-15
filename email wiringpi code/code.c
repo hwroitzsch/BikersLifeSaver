fc9fadb5f4c1bc57b4c1695c73111c0ac2963a22
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2015-12-15 10:30:58 +0100

    class name changed to WarningLevel

--- email wiringpi code/code.c
+++ email wiringpi code/code.c
@@ -1,15 +1,5 @@
-/*
-From the command-line:
-
-  gpio -x mcp3004:200:0 aread 200
-
-That should read the first channel on the 3008 - the 3004 is the same chip with just 4 channels. Assumes it's on SPI bus 0. (the trailing :0)
-
-In code:
-*/
-
 #include <stdio.h>
-#include <wiringPi.h
+#include <wiringPi.h>
 
 #define BASE 200
 #define SPI_BUS 0
@@ -20,3 +10,13 @@ main ()
 	mcp3004Setup(BASE, SPI_BUS);
 	printf("Bus SPI_BUS is: %d\n", analogRead(BASE));
 }
+
+/*
+From the command-line:
+
+  gpio -x mcp3004:200:0 aread 200
+
+That should read the first channel on the 3008 - the 3004 is the same chip with just 4 channels. Assumes it's on SPI bus 0. (the trailing :0)
+
+In code:
+*/
--- opencv_direction_indicator_detection/oop/WarningLevel.py
+++ opencv_direction_indicator_detection/oop/WarningLevel.py
@@ -3,7 +3,7 @@ from enum import Enum
 __author__ = 'Hans-Werner Roitzsch'
 
 
-class Color(Enum):
+class WarningLevel(Enum):
 	NO_WARNING = 0
 	HAZARD_SPOT_WARNING = 1
 	DANGEROUS_SITUATION_WARNING = 2
