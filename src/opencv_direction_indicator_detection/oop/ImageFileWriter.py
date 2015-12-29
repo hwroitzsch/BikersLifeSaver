adec567fb408219b675224254157da0484b90a72
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2015-12-29 15:10:51 +0100

    added print to print image_data shape

--- src/opencv_direction_indicator_detection/oop/ImageFileWriter.py
+++ src/opencv_direction_indicator_detection/oop/ImageFileWriter.py
@@ -19,10 +19,12 @@ class ImageFileWriter:
 				file_path = os.path.join(image_directory, file_name)
 				print('writing image file to', file_path)
 
+				print('IMAGE_DATA.SHAPE:', image_data.shape)
+
 				if image_data.shape is not None:
 					converted_image_data = opencv.cvtColor(image_data, opencv.COLOR_RGB2BGR)  # images might need to be converted to be correctly written by opencv
 				else:
 					print('image_data.shape is None')
-					
+
 				opencv.imwrite(file_path, converted_image_data, (opencv.IMWRITE_PNG_COMPRESSION, 0))
 				index += 2
