2f713b4e86f15e95abc9d7e911cf39d8fd58e4ba
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2016-01-08 16:19:36 +0100

    debug code

--- src/opencv_direction_indicator_detection/oop/examples/read_gps_sensor_data.py
+++ src/opencv_direction_indicator_detection/oop/examples/read_gps_sensor_data.py
@@ -1,34 +1,39 @@
-import gps.gps as gps3
-from gps import WATCH_ENABLE, WATCH_NEWSTYLE
-# from gps import gps as gps3
+import gps
+
+print(dir(gps))
+
+
+
 # from gps import WATCH_ENABLE, WATCH_NEWSTYLE
-
-# Listen on port 2947 (gpsd) of localhost
-print('gps3:')
-print(dir(gps3))
-
-print('gps3.gps:')
-print(dir(gps3.gps))
-
-session = gps3.gps("localhost", "2947")
-session.stream(WATCH_ENABLE | WATCH_NEWSTYLE)
-
-while True:
-	try:
-		report = session.next()
-		# Wait for a 'TPV' report and display the current time
-		# To see all report data, uncomment the line below
-		# print report
-		if report['class'] == 'TPV':
-			if hasattr(report, 'time'):
-				print('TIME:', report.time)
-
-	except KeyError:
-		pass
-
-	except KeyboardInterrupt:
-		quit()
-
-	except StopIteration:
-		session = None
-		print("GPSD has terminated")
+# # from gps import gps as gps3
+# # from gps import WATCH_ENABLE, WATCH_NEWSTYLE
+#
+# # Listen on port 2947 (gpsd) of localhost
+# print('gps3:')
+# print(dir(gps3))
+#
+# print('gps3.gps:')
+# print(dir(gps3.gps))
+#
+# session = gps3.gps("localhost", "2947")
+# session.stream(WATCH_ENABLE | WATCH_NEWSTYLE)
+#
+# while True:
+# 	try:
+# 		report = session.next()
+# 		# Wait for a 'TPV' report and display the current time
+# 		# To see all report data, uncomment the line below
+# 		# print report
+# 		if report['class'] == 'TPV':
+# 			if hasattr(report, 'time'):
+# 				print('TIME:', report.time)
+#
+# 	except KeyError:
+# 		pass
+#
+# 	except KeyboardInterrupt:
+# 		quit()
+#
+# 	except StopIteration:
+# 		session = None
+# 		print("GPSD has terminated")
